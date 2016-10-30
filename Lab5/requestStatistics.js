3a8b7a47ce256c030bc6f84c0d7cba88d14ed026
Sych Yuriy <sichyuriyy@gmail.com>
2016-10-30 19:56:22 +0200

    added some statistics

---
+++ Lab5/requestStatistics.js
@@ -0,0 +1,39 @@
+var searchSystemRequests = {
+  "yandex" : [
+    {
+      conent : "Ukrainian dishes",
+      a : 8,
+      b : 2,
+      c : 5, 
+      d : 0
+    }
+  ],
+  "mail.ru" : [
+    {
+      conent : "Ukrainian dishes",
+      a : 7,
+      b : 3,
+      c : 8, 
+      d : 0
+    }
+  ],
+  "yahoo" : [
+    {
+      conent : "Ukrainian dishes",
+      a : 8,
+      b : 2,
+      c : 3, 
+      d : 0
+    }
+  ],
+  "bing" : [
+    {
+      conent : "Ukrainian dishes",
+      a : 10,
+      b : 0,
+      c : 2, 
+      d : 0
+    }
+  ]
+   
+};
--- Lab5/script.js
+++ Lab5/script.js
@@ -10,3 +10,56 @@ function search(frameId, searchSystem, searchStr) {
   searchUrl = searchSystems_urls[searchSystem];
   document.getElementById(frameId).src = searchUrl + searchStr;
 }
+
+var statistics = searchSystemRequests;
+
+function getRecall(searchSystem) {
+  var r_sum = 0;
+  var r_count = 0;
+  var requests = statistics[searchSystem];
+  for (var i = 0; i < requests.length; i++) {
+    var request = requests[i];
+    r_sum += request.a / (request.a + request.c);
+    r_count++;
+  }
+  return r_sum / r_count;
+}
+
+function getPrecision(searchSystem) {
+  var p_sum = 0;
+  var p_count = 0;
+  var requests = statistics[searchSystem];
+  for (var i = 0; i < requests.length; i++) {
+    var request = requests[i];
+    p_sum += request.a / (request.a + request.b);
+    p_count++;
+  }
+  return p_sum / p_count;
+}
+
+function getAccuracy(searchSystem) {
+  var acc_sum = 0;
+  var acc_count = 0;
+  var requests = statistics[searchSystem];
+  for (var i = 0; i < requests.length; i++) {
+    var request = requests[i];
+    acc_sum += (request.a + request.d) / (request.a + request.b + request.c + request.d);
+    acc_count++;
+  }
+  return acc_sum / acc_count;
+}
+
+function getError(searchSystem) {
+  var err_sum = 0;
+  var err_count = 0;
+  var requests = statistics[searchSystem];
+  for (var i = 0; i < requests.length; i++) {
+    var request = requests[i];
+    err_sum += (request.b + request.c) / (request.a + request.b + request.c + request.d);
+    err_count++;
+  }
+  return err_sum / err_count;
+}
+
+
+
